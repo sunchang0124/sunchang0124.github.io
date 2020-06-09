d5c8c2b07108007adaeb81b2d509cf758e0d03c0
Chang Sun <changsun@Vincents-MacBook-Pro.local>
2020-06-09 17:32:36 +0200

    Fetch SOLID Data

--- src/index.js
+++ src/index.js
@@ -3,18 +3,31 @@ import { fetchDocument } from 'tripledoc';
 import { foaf } from 'rdf-namespaces';
 
 const btns = document.querySelectorAll(".btn");
+// Log out if user has been login on this machine
+auth.logout();
 
 async function getWebId() {
+
+  const logStatus = document.getElementById("logStatus");
+  const headings = document.getElementById("headings");
+  const fetch = document.getElementById("fetch");
   /* 1. Check if we've already got the user's WebID and access to their Pod: */
+
   let session = await auth.currentSession();
   if (session) {
+    logStatus.textContent = "Log Out";
+    headings.textContent = "Fetch Data from Solid (click 'Fetch' button)";
+    fetch.textContent = "Fetch";
     return session.webId;
   }
-  /* 2. User has not logged in; ask for their Identity Provider: */
-  const identityProvider = await getIdentityProvider();
-
-  /* 3. Initiate the login process - this will redirect the user to their Identity Provider: */
-  auth.login(identityProvider);
+  else{
+    logStatus.textContent = "Log In";
+    headings.textContent = "Login with Your Identity Provider: ";
+    /* 2. User has not logged in; ask for their Identity Provider: */
+    const identityProvider = await getIdentityProvider();
+    /* 3. Initiate the login process - this will redirect the user to their Identity Provider: */
+    auth.login(identityProvider);
+  } 
 }
 
 function getIdentityProvider() {
@@ -48,46 +61,72 @@ getWebId().then(webId => {
   const webIdElement = document.getElementById("webId");
   webIdElement.textContent = "Your WebID is: "+ webId;
   const webIdDisplay = document.getElementById("webIdDisplay");
-  // webIdDisplay.style.display = "initial";
+  webIdDisplay.style.display = "initial";
   if (webId){
-    alert('Welcome!');
+    alert('Welcome, ' + webId);
   }
-
-  btns.forEach(function(btn) {
-    btn.addEventListener("click", function(e){
-      e.preventDefault();
-      if (e.currentTarget.classList.contains('logout')) {
-        console.log("Log out!");
-        auth.logout().then(() => alert('Goodbye!'));
-      };
-    });
-  });
 });
 
-async function getName(webId) {
-  /* 1. Fetch the Document at `webId`: */
-  const webIdDoc = await fetchDocument("https://chang.inrupt.net/public/apptest.ttl");
-  /* 2. Read the Subject representing the current user's profile: */
-  const profile = webIdDoc.getSubject("https://chang.inrupt.net/profile/card#me");
-  // const profile = webIdDoc.getSubject("https://chang.inrupt.net/public/apptest.ttl"); 
-  /* 3. Get their foaf:name: */
+// Get Data From SOLID Pod
+async function getTriplesObjects(fetchFrom, fetchSubject, fetchPredicate, option) {
 
-  // console.log(webIdDoc_te.getTriples("https://chang.inrupt.net/profile/card#me")); 
-  console.log(profile.getAllRefs()); 
-  console.log(profile.getAllLiterals()); 
-  console.log(profile.getInteger("http://schema.org/height"));
-  console.log(profile.getString("http://xmlns.com/foaf/0.1/name"));
-  // console.log(profile.getInteger(foaf.age))
-  return "abc"
+  /* 1. Fetch the Document at `webId`: */
+  try{
+    const fetchFromDoc = await fetchDocument(fetchFrom);
+  }
+  catch(err) {
+    alert(err.message) ;
+  }
+  finally{
+    
+    const fetchFromDoc = await fetchDocument(fetchFrom);
+
+    if (option){
+      /* 2. Read the Subject representing the current user's profile: */
+      const getTriples = fetchFromDoc.getTriples(fetchFrom);
+      return getTriples
+    }
+    
+    else{
+      /* 3. Get their triples or objects */
+      const getSubject = fetchFromDoc.getSubject(fetchSubject);
+      if (fetchPredicate) {
+        const getObjects = getSubject.getAllLiterals(fetchPredicate).concat(getSubject.getAllRefs(fetchPredicate));
+        return getObjects
+      }
+      else{
+        const getObjects = getSubject.getTriples()
+        return getObjects
+      }
+    }
+  }
 }
 
-getName(webId).then(profileString => {
-  btns.forEach(function(btn) {
-    btn.addEventListener("click", function(e){
-      e.preventDefault();
-      if (e.currentTarget.classList.contains('fetch')) {
-        console.log(profileString);
-      };
+
+btns.forEach(function(btn) {
+  btn.addEventListener("click", function(e){
+    e.preventDefault();
+    const fetchFrom = document.getElementById("fetchFrom").value;
+    const fetchSubject = document.getElementById("fetchSubject").value;
+    const fetchPredicate = document.getElementById("fetchPredicate").value;
+    const getTriplesOption = e.currentTarget.classList.contains('fetchTriples')
+    
+    getTriplesObjects(fetchFrom, fetchSubject, fetchPredicate, getTriplesOption).then(getFetchedData => {
+      const fetchedText = document.getElementById("fetchedText");
+      fetchedText.setAttribute('style', 'white-space: pre;');
+      
+      let printString = '';
+      if (getTriplesOption || !fetchPredicate){
+        for (let i = 0; i < getFetchedData.length; i++){
+          printString += getFetchedData[i].subject.id + '\r\t' + getFetchedData[i].predicate.id + '\r\t' + getFetchedData[i].object.id + '. \r\n' ;
+        }
+      }
+      else if (!getTriplesOption && fetchSubject && fetchPredicate){
+        for (let i = 0; i < getFetchedData.length; i++){
+          printString += getFetchedData[i] + '\r\n' ;
+        }
+      }
+      fetchedText.textContent = printString;
     });
   });
 });
